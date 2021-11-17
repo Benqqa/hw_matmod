e69840715d1779d29c44c31197e3c43c3f5dc8b6
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-11-18 00:46:37 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -32,7 +32,7 @@ int main() {
                     }
                 }
     }
-    //in.close();     // закрываем файл
+    in.close();     // закрываем файл
     std::cout << "Leftmost: " << fx_l << " " << fy_l << "\n";
     std::cout << "Rightmost: " << fx_r << " " << fy_r << "\n";
 
