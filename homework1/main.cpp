cc82c2782de91307564e07ed42f2b3856cf3aa18
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-11-18 01:10:16 +0300

    fix

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,39 +1,13 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
 #include <valarray>
 
 using namespace std;
-/*
-string * StringToMass(string base_str, char delim, int size){
 
-    size_t pos = 0;
-    size_t base_str_size = base_str.size();
-    //size_t delim_size = delim.size();
-    size_t delim_size=1;
-    string temp;
-    int i=0;
-    string *mass_str = new string[size];
-    while (pos < base_str_size) {
-        temp = temp.assign(base_str, pos, base_str.find(delim, pos) - pos);
-        if (temp.size() > 0)  // проверка на пустую строку при необходимости
-        {
-           // cout << temp << endl;
-            mass_str[i] = temp;
-            i++;
-            if(i==size){
-                break;
-            }
-            //cout << i << endl;
-        }
-        pos += temp.size() + delim_size;
 
-    }
-
-    return mass_str;
-}*/
 int main() {
     int xn,yn,fx_r,fy_r,fx_l,fy_l;
+    int a=0; int b=0;
     double max_d_r=0;
     double max_d_l=0;
     std::ifstream in("in.txt"); // окрываем файл для чтения
@@ -42,19 +16,23 @@ int main() {
         in >> x >> y;
         xn=(-1)*x;
         yn=(-1)*y;
+        a=x;
+        b=y;
         //std::<<  <<::endl;
         double dist;
         while (in >> x >> y)
         {
                 //растояние
-            (xn !=0 && yn !=0)? dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn))) :(xn ==0)? dist =x:dist= y;
-               /* if(xn !=0 && yn !=0){
-                    dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn)));
-					dist = std::round(dist * 10000000000.0) / 10000000000.0;
+
+                if(xn !=0 && yn !=0){
+                   // dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn)));
+                    double len = std::sqrt((xn ) * (xn) + (yn) * (yn));
+                    dist = std::abs((yn ) * x - (xn) * y ) / len;
+					//dist = std::round(dist * 10000000000.0) / 10000000000.0;
                 }
                 else{
                     (xn ==0)? dist =x:dist= y;
-                }*/
+                }
                 //<0 - справа:
                 if(((xn-0)*(y-0)-(yn-0)*(x-0))>=0){
                     if(dist>=max_d_r){
