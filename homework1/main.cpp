e0f64f63f78f014fc269a93c9c1e267d8e41528a
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-11-18 01:15:43 +0300

    revert

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,37 +1,73 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <valarray>
 
 using namespace std;
+string * StringToMass(string base_str, char delim, int size){
 
+    size_t pos = 0;
+    size_t base_str_size = base_str.size();
+    //size_t delim_size = delim.size();
+    size_t delim_size=1;
+    string temp;
+    int i=0;
+    string *mass_str = new string[size];
+    while (pos < base_str_size) {
+        temp = temp.assign(base_str, pos, base_str.find(delim, pos) - pos);
+        if (temp.size() > 0)  // проверка на пустую строку при необходимости
+        {
+           // cout << temp << endl;
+            mass_str[i] = temp;
+            i++;
+            if(i==size){
+                break;
+            }
+            //cout << i << endl;
+        }
+        pos += temp.size() + delim_size;
 
+    }
+
+    return mass_str;
+}
+std::tuple<int, int> step(int dividend, int divisor) {
+    return  std::make_tuple(dividend / divisor, dividend % divisor);
+}
 int main() {
-    int xn,yn,fx_r,fy_r,fx_l,fy_l;
-    int a=0; int b=0;
+    std::string line;
+    double xn,yn,fx_r,fy_r,fx_l,fy_l;
+    double finde_value=1;
     double max_d_r=0;
     double max_d_l=0;
+    bool isFirstLine= true;
+    bool isTwiceLine_r= true,isTwiceLine_l= true;
     std::ifstream in("in.txt"); // окрываем файл для чтения
-
-        int x=0; int y=0;
+    if (in.is_open())
+    {
+        double x=0; double y=0;
         in >> x >> y;
         xn=(-1)*x;
         yn=(-1)*y;
-        a=x;
-        b=y;
         //std::<<  <<::endl;
-        double dist;
         while (in >> x >> y)
         {
                 //растояние
-
+                double dist;
                 if(xn !=0 && yn !=0){
-                   // dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn)));
-                    double len = std::sqrt((xn ) * (xn) + (yn) * (yn));
-                    dist = std::abs((yn ) * x - (xn) * y ) / len;
-					//dist = std::round(dist * 10000000000.0) / 10000000000.0;
+                    dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn)));
+					dist = std::round(dist * 10000000000.0) / 10000000000.0;
+                   // dist=(abs((yn-0)*x-(xn-0)*y+xn*0-yn*0))/(sqrt(pow(xn-0,2)+pow(yn-0,2))); //вектор как прямая заданная 2мя точками
                 }
                 else{
                     (xn ==0)? dist =x:dist= y;
+                    /*if(xn ==0){
+                        dist= x;
+                        //dist=sqrt(pow(x-xn,2)+pow(y-yn,2)); //как растояние между 2мя точками
+                    }
+                    else{
+                        dist=y;
+                    }*/
                 }
                 //<0 - справа:
                 if(((xn-0)*(y-0)-(yn-0)*(x-0))>=0){
@@ -40,6 +76,15 @@ int main() {
                         fx_r=x;
                         fy_r=y;
                     }
+                    /*if(isTwiceLine_r){
+                        isTwiceLine_r= false;
+                        max_d_r=dist;
+                        fx_r=x;
+                        fy_r=y;
+                    }
+                    else{
+
+                    }*/
                 }//>0 - слева
                 else{
                     if(dist>=max_d_l){
@@ -47,7 +92,19 @@ int main() {
                         fx_l=x;
                         fy_l=y;
                     }
+
+                    /*if(isTwiceLine_l){
+                        isTwiceLine_l= false;
+                        max_d_l=dist;
+                        fx_l=x;
+                        fy_l=y;
+                    }
+                    else{
+                    }*/
                 }
+
+            //delete[] values;
+        }
     }
     in.close();     // закрываем файл
     std::cout << "Leftmost: " << fx_l << " " << fy_l << "\n";
