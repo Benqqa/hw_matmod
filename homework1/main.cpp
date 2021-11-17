ef64c49e9833f7a3dcc78fa49cd6101301988497
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-11-18 00:50:27 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -33,7 +33,6 @@ string * StringToMass(string base_str, char delim, int size){
     return mass_str;
 }*/
 int main() {
-    std::string line;
     int xn,yn,fx_r,fy_r,fx_l,fy_l;
     double max_d_r=0;
     double max_d_l=0;
@@ -48,14 +47,14 @@ int main() {
         while (in >> x >> y)
         {
                 //растояние
-
-                if(xn !=0 && yn !=0){
+            (xn !=0 && yn !=0)? dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn))) :(xn ==0)? dist =x:dist= y;
+               /* if(xn !=0 && yn !=0){
                     dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn)));
 					dist = std::round(dist * 10000000000.0) / 10000000000.0;
                 }
                 else{
                     (xn ==0)? dist =x:dist= y;
-                }
+                }*/
                 //<0 - справа:
                 if(((xn-0)*(y-0)-(yn-0)*(x-0))>=0){
                     if(dist>=max_d_r){
