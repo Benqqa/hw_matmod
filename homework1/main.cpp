a7b6f781972edd2f750ca5b92b18a7af82e63251
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-12-09 01:14:49 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -6,31 +6,25 @@ int main() {
     double max_d_r=0;
     double max_d_l=0;
     std::ifstream in("in.txt"); // окрываем файл для чтения
-   /* if (in.is_open())
-    {*/
-        int x=0; int y=0;
-        in >> xn >> yn;
-        double dist;
-        while (in >> x >> y)
-        {
-                if(x==0 && y==0){
-                    continue;
-                }
-                //растояние
-                dist = fabs(xn*x-yn*y)/sqrt(xn*xn+yn*yn);
-                //<0 - справа:
-                if((xn*y-yn*x)<=0 && dist>=max_d_r){
-                    max_d_r=dist;
-                    fx_r=x;
-                    fy_r=y;
-                }//>0 - слева
-                if( (xn*y-yn*x)>0 && dist>=max_d_l){
-                    max_d_l=dist;
-                    fx_l=x;
-                    fy_l=y;
-                }
-        }
-    //}
+    int x=0; int y=0;
+    in >> xn >> yn;
+    double dist;
+    while (in >> x >> y)
+    {
+            //растояние
+            dist = fabs(xn*x-yn*y)/sqrt(xn*xn+yn*yn);
+            //<0 - справа:
+            if((xn*y-yn*x)<=0 && dist>=max_d_r){
+                max_d_r=dist;
+                fx_r=x;
+                fy_r=y;
+            }//>0 - слева
+            if( (xn*y-yn*x)>0 && dist>=max_d_l){
+                max_d_l=dist;
+                fx_l=x;
+                fy_l=y;
+            }
+    }
     in.close();     // закрываем файл
     std::cout << "Leftmost: " << fx_l << " " << fy_l << "\n";
     std::cout << "Rightmost: " << fx_r << " " << fy_r << "\n";
