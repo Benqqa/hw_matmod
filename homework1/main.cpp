0a7e55aeab10d6be2fc789fb0b3a6cc40caa7cce
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-12-09 00:27:37 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
 #include <valarray>
 
 using namespace std;
 int main() {
     std::string line;
-    double xn,yn,fx_r,fy_r,fx_l,fy_l;
+    int xn,yn,fx_r,fy_r,fx_l,fy_l;
     double max_d_r=0;
     double max_d_l=0;
     std::ifstream in("in.txt"); // окрываем файл для чтения
     if (in.is_open())
     {
-        double x=0; double y=0;
+        int x=0; int y=0;
         in >> x >> y;
         xn=(-1)*x;
         yn=(-1)*y;
@@ -22,19 +21,17 @@ int main() {
                 double dist;
                 if(xn !=0 && yn !=0){
                     dist = abs(xn*x-yn*y)/sqrt(xn*xn+yn*yn);
-                   // dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn)));
-					//dist = std::round(dist * 10000000000.0) / 10000000000.0;
                 }
                 else{
                     (xn ==0)? dist =x:dist= y;
                 }
                 //<0 - справа:
-                if(((xn-0)*(y-0)-(yn-0)*(x-0))>=0 && dist>=max_d_r){
+                if((xn*y-yn*x)>=0 && dist>=max_d_r){
                         max_d_r=dist;
                         fx_r=x;
                         fy_r=y;
                 }//>0 - слева
-                if( ((xn-0)*(y-0)-(yn-0)*(x-0))<0 && dist>=max_d_l){
+                if( (xn*y-yn*x)<0 && dist>=max_d_l){
                     max_d_l=dist;
                     fx_l=x;
                     fy_l=y;
